15ea09e26a2f80625d24bb90f82fade32eabc17b
marco76tv <marco.sottana@gmail.com>
2025-03-26 09:37:08 +0100

    first

---
+++ lang/it/activity.php
@@ -0,0 +1,82 @@
+<?php return array (
+  'table' => 
+  array (
+    'heading' => 'Attività',
+  ),
+  'fields' => 
+  array (
+    'nome' => 
+    array (
+      'label' => 'Nome',
+    ),
+    'tipo' => 
+    array (
+      'label' => 'Tipo',
+    ),
+    'appartiene_a_liquidazione_a_fasi' => 
+    array (
+      'label' => 'Appartiene a liquidazione a fasi?',
+    ),
+    'liquidazione_fasi' => 
+    array (
+      'label' => 'Fasi di liquidazione',
+    ),
+    'quota_percentuale' => 
+    array (
+      'label' => 'Quota percentuale',
+    ),
+    'importo' => 
+    array (
+      'label' => 'Importo',
+    ),
+    'anno_competenza' => 
+    array (
+      'label' => 'Anno competenza',
+    ),
+    'project_id' => 
+    array (
+      'label' => 'ID Progetto',
+    ),
+    'employees' => 
+    array (
+      'cognome' => 
+      array (
+        'label' => 'Dipendenti',
+      ),
+    ),
+    'project' => 
+    array (
+      'nome' => 
+      array (
+        'label' => 'Nome Progetto',
+      ),
+    ),
+    'phase_id' => 
+    array (
+      'label' => 'phase_id',
+    ),
+    'toggleColumns' => 
+    array (
+      'label' => 'toggleColumns',
+    ),
+    'reorderRecords' => 
+    array (
+      'label' => 'reorderRecords',
+    ),
+    'resetFilters' => 
+    array (
+      'label' => 'resetFilters',
+    ),
+    'applyFilters' => 
+    array (
+      'label' => 'applyFilters',
+    ),
+  ),
+  'navigation' => 
+  array (
+    'label' => 'Attività',
+    'group' => 'Incentivi',
+    'icon' => 'incentivi-activity',
+    'sort' => 4,
+  ),
+);
