eccd8076a1320b6c4f6ab35ad42d8a4264883681
marco xot <marco.sottana@gmail.com>
2025-04-01 14:17:06 +0200

    .

--- laravel/Modules/Performance/app/Filament/Resources/OrganizzativaResource/Pages/ListOrganizzativas.php
+++ laravel/Modules/Performance/app/Filament/Resources/OrganizzativaResource/Pages/ListOrganizzativas.php
@@ -6,46 +6,39 @@ namespace Modules\Performance\Filament\Resources\OrganizzativaResource\Pages;
 
 use Filament\Actions;
 use Filament\Tables;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\SplitColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Support\Arr;
 use Modules\Performance\Filament\Resources\OrganizzativaResource;
+use Modules\Performance\Models\Organizzativa;
 use Modules\Xot\Filament\Actions\Header\ExportXlsAction;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
+use function Safe\date;
+
+/**
+ * ---.
+ */
 class ListOrganizzativas extends XotBaseListRecords
 {
     protected static string $resource = OrganizzativaResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-            \Modules\Ptv\Filament\Actions\Header\CopyFromLastYearAction::make(),
-            \Modules\Ptv\Filament\Actions\Header\PopulateYearAction::make(),
-            ExportXlsAction::make(),
-        ];
-    }
-
+    /**
+     * @return array<string, TextColumn|SplitColumn>
+     */
     public function getListTableColumns(): array
     {
         return [
             'id' => Tables\Columns\TextColumn::make('id')
                 ->sortable(),
-            'ha_diritto' => Tables\Columns\ToggleColumn::make('ha_diritto')
-                ->sortable(),
-            'motivo' => Tables\Columns\TextColumn::make('motivo')
-                ->wrap()
-                ->searchable(),
-            'matr' => Tables\Columns\TextColumn::make('matr')
-                ->sortable()
-                ->searchable(),
             'cognome' => Tables\Columns\TextColumn::make('cognome')
                 ->sortable()
-                ->searchable(),
-            'nome' => Tables\Columns\TextColumn::make('nome')
-                ->sortable()
-                ->searchable(),
-            'email' => Tables\Columns\TextColumn::make('email')
-                ->sortable()
-                ->searchable(),
+                ->searchable()
+                ->description(fn (Organizzativa $record): string => 
+                    "{$record->nome} - {$record->matr} - {$record->email}"
+                ),
             'stabi' => Tables\Columns\TextColumn::make('stabi')
                 ->sortable()
                 ->searchable(),
@@ -63,6 +56,34 @@ class ListOrganizzativas extends XotBaseListRecords
                 ->dateTime()
                 ->sortable()
                 ->toggleable(isToggledHiddenByDefault: true),
+        ];
+    }
+
+    /**
+     * @return array<string, Actions\CreateAction>
+     */
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+            \Modules\Ptv\Filament\Actions\Header\CopyFromLastYearAction::make(),
+            \Modules\Ptv\Filament\Actions\Header\PopulateYearAction::make(),
+            ExportXlsAction::make(),
+        ];
+    }
+
+    /**
+     * @return array<string, SelectFilter>
+     */
+    public function getTableFilters(): array
+    {
+        return [
+            'anno' => SelectFilter::make('anno')
+                ->options(Arr::pluck(Organizzativa::select('anno')->distinct()->get(), 'anno', 'anno')),
+            'stabi' => SelectFilter::make('stabi')
+                ->options(Arr::pluck(Organizzativa::select('stabi')->distinct()->get(), 'stabi', 'stabi')),
+            'repar' => SelectFilter::make('repar')
+                ->options(Arr::pluck(Organizzativa::select('repar')->distinct()->get(), 'repar', 'repar')),
         ];
     }
 }
