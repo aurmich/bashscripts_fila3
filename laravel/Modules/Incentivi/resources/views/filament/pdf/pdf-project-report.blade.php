ea02c83718fb330015cc4f47fe387f64ad606a7c
marco xot <marco.sottana@gmail.com>
2025-04-09 09:27:55 +0200

    .

--- laravel/Modules/Incentivi/resources/views/filament/pdf/pdf-project-report.blade.php
+++ laravel/Modules/Incentivi/resources/views/filament/pdf/pdf-project-report.blade.php
@@ -3,10 +3,7 @@
 <head>
     <title>Resoconto Progetto</title>
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-    {{-- <link rel="stylesheet" href="{{ $_theme->asset('ptv::dist/assets/app.css') }}"> --}}
-    {{ $_theme->asset('ptv::dist/assets/app.css') }}
-die();
-    <link rel="stylesheet" href="{{ Module::asset('Ptv:resources/dist/assets/app.css') }}">
+    <link rel="stylesheet" href="{{ $_theme->asset('Ptv::dist/assets/app.css') }}">
     
 </head>
 
