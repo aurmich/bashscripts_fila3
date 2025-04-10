fb29b61d76d626bf36ce2a7c46643653b2c31757
marco76tv <marco.sottana@gmail.com>
2025-04-10 10:45:26 +0200

    .

--- git_push_subtree.sh
+++ git_push_subtree.sh
@@ -101,4 +101,4 @@ push_subtree() {
 # Run sync
 push_subtree
 
-echo "Subtree $LOCAL_PATH synchronized successfully with $REMOTE_REPO"
+echo "👍 Subtree $LOCAL_PATH pushed successfully with $REMOTE_REPO"
