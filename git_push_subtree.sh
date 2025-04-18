9b62663f60be898a0f699c95d8d0f22b05476e43
marco76tv <marco.sottana@gmail.com>
2025-04-18 18:27:22 +0200

    .

--- git_push_subtree.sh
+++ git_push_subtree.sh
@@ -45,7 +45,7 @@ push_subtree() {
     ############################################
     git fetch "$REMOTE_REPO" "$BRANCH"
     git subtree split --rejoin --prefix="$LOCAL_PATH" -b "$TEMP_BRANCH" || log "❌ Failed to split subtree"
-    git push -f "$REMOTE_REPO"  "$TEMP_BRANCH":"$BRANCH" || log "❌ Failed to push subtree"
+    git push  "$REMOTE_REPO"  "$TEMP_BRANCH":"$BRANCH" || log "❌ Failed to push subtree"
     git branch -D "$TEMP_BRANCH" || log "❌ Failed to delete temporary branch $TEMP_BRANCH"
 
     git rebase --rebase-merges --strategy subtree "$BRANCH" --autosquash
