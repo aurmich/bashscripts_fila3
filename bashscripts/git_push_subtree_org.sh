505bf94dff802d0baa2961f5d68f11a0ade91aa4
marco76tv <marco.sottana@gmail.com>
2025-04-22 11:50:09 +0200

    .

--- bashscripts/git_push_subtree_org.sh
+++ bashscripts/git_push_subtree_org.sh
@@ -2,14 +2,14 @@
 
 source ./bashscripts/lib/custom.sh
 # Validate input
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <path> <remote_repo> <branch>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <path> <remote_repo> "
     exit 1
 fi
 
 LOCAL_PATH="$1"
 REMOTE_REPO="$2"
-BRANCH="$3"
+
 curr_dir=$(pwd)
 
 echo "🔄 Submodule $LOCAL_PATH"
@@ -23,7 +23,7 @@ git remote add origin "$REMOTE_REPO"
 git fetch --all
 git add -A
 git commit -am .
-git merge origin/"$BRANCH" --allow-unrelated-histories
+git pull origin/"$BRANCH" --allow-unrelated-histories --autostash
 git add -A
 git commit -am .
 git push -u origin "$BRANCH"
