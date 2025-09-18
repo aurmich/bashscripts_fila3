847394151f2c19cb23614a095a15a0f7ea8b1eb9
marco76tv <marco.sottana@gmail.com>
2025-09-18 11:11:35 +0200

    .

--- app/Services/ArtisanService.php
+++ app/Services/ArtisanService.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 namespace Modules\Xot\Services;
@@ -285,3 +286,39 @@ class ArtisanService
         }*/
     }
 }
+=======
+namespace App\Services;
+
+use App\Traits\CommandTrait;
+use Illuminate\Support\Facades\Log;
+
+class ArtisanService
+{
+    use CommandTrait;
+
+    private string $laravelDir;
+
+    public function __construct(array $config)
+    {
+        $this->laravelDir = $config['env']['LARAVEL_DIR'];
+    }
+
+    /**
+     * Esegue un comando artisan
+     */
+    public function run(string $command, array $options = []): array
+    {
+        $artisanPath = $this->laravelDir . '/artisan';
+        
+        if (!file_exists($artisanPath)) {
+            return [
+                'success' => false,
+                'output' => 'File artisan non trovato',
+                'status' => 1
+            ];
+        }
+
+        return $this->executeCommand('php ' . $artisanPath . ' ' . $command, $options);
+    }
+} 
+>>>>>>> 030233b5 (Squashed 'public_html/noconsole/' content from commit dfb975a22)
